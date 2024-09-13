e66f63b301fc5ac4b0a35a20edf0ecc25262a4e7
GermanMorenoBauer <102313750+GermanMorenoBauer@users.noreply.github.com>
2024-09-13 08:42:05 -0300

    Delete entities directory

--- entities/casa.js
+++
@@ -1,9 +0,0 @@
-import { Inmueble } from "./inmueble";
-
-export class Casa extends Inmueble{
-
-
-    constructor(valor, tamanio, ambientes, operacion, zona){
-        super(valor, tamanio, ambientes, operacion, zona);
-    }
-}
--- entities/empleado.js
+++
@@ -1,9 +0,0 @@
-import { Persona } from "./Persona";
-
-export class Empleado extends Persona{
-    comisiones;
-
-    constructor(nombre, dni){
-        super(nombre, dni);
-    }
-}
